28cdc1c7d62c92b078a2533e5bc42c31cd7e1f0b
manojjami09 <manojjami09@gmail.com>
2025-06-09 20:29:55 +0530

    Angular-Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-Form';
+  name: string = "";
+  subject: string = "";
+  testScore1: number | null = null;
+  testScore2: number | null = null;
+  finalMessage = "";
+
+  showMessage(){
+     const total = (this.testScore1 || 0) + (this.testScore2 || 0);
+     this.finalMessage = `Hello ${this.name}! Your Score in ${this.subject} is ${total}`;
+  }
+
 }
